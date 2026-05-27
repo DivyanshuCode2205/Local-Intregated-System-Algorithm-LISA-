ad51844769f860b2038780f5830b62268bac901c
DivyanshuCode2205 <divyanshuxd3@gmail.com>
2026-05-27 11:52:07 +0530

    Changed the directories according to the new laptop.

--- files_library.py
+++ files_library.py
@@ -1,13 +1,13 @@
 import os
 
-my_files = {"downloads": os.path.normpath(os.path.join(os.path.expanduser("~"), "Downloads")),
-    "documents": os.path.normpath(os.path.join(os.path.expanduser("~"), "Documents")),
-    "vs code":os.path.normpath(r"C:\Users\DELLS\AppData\Local\Programs\Microsoft VS Code\Code.exe"),
-    "quick share":os.path.normpath(r"C:\ProgramData\Microsoft\Windows\Start Menu\Quick Share.lnk"),
+my_files = {"downloads": os.path.normpath("D:\Downloads"),
+    "documents": os.path.normpath("D:\Documents"),
+    "vs code":os.path.normpath(r"C:\Users\Divyanshu xd\AppData\Local\Programs\Microsoft VS Code\Code.exe"),
+    "quick share":os.path.normpath(r"C:\Program Files\Google\NearbyShare\nearby_share.exe"),
     "desktop": os.path.normpath(os.path.join(os.path.expanduser("~"), "Desktop")),
-    "pictures": os.path.normpath(os.path.join(os.path.expanduser("~"), "Pictures")),
-    "saved pictures":os.path.normpath(os.path.join(os.path.expanduser('~'), 'Pictures', 'Saved Pictures')),
-    "notes": os.path.normpath(os.path.join(os.path.expanduser('~'), "Desktop", "PW Class Notes")),
-    "screenshots":os.path.normpath(os.path.join(os.path.expanduser('~'), 'Pictures', 'Screenshots')),
+    "pictures": os.path.normpath("D:\Pictures"),
+    "saved pictures":os.path.normpath(os.path.join("D:\Pictures", 'Saved Pictures')),
+    "screenshots":os.path.normpath(os.path.join("D:\Pictures", 'Screenshots')),
+    "wallpapers":os.path.normpath(os.path.join("D:\Pictures", "Wallpapers")),
     "c drive":os.path.normpath("C:/"), "d drive":os.path.normpath("D:/"),
     "password generator":os.path.normpath(r"D:\Python Programming\Chapter 7 Practice-set\password_generator.py")}
